142b4819966dc63549dceda2d11edc0a23ee29e0
Vytautas Strimaitis <vstrimaitis@gmail.com>
2020-12-10 08:05:25 +0200

    Improve day 10

--- 10/sol.py
+++ 10/sol.py
@@ -1,43 +1,29 @@
 from puzzle import PuzzleContext
+from collections import defaultdict
 
 
 with PuzzleContext(year=2020, day=10) as ctx:
     ans1 = None
     ans2 = None
 
-    nums = set(int(x) for x in ctx.nonempty_lines)
+    # Part 1
+    arr = [int(x) for x in ctx.nonempty_lines]
+    arr.append(0)
+    arr.append(max(arr) + 3)
 
-    curr = 0
-    mx = max(nums) + 3
-    cnts = dict()
-    cnts[1] = 0
-    cnts[2] = 0
-    cnts[3] = 0
-    nums.add(mx)
-    while True:
-        found = False
-        for d in range(1, 3+1):
-            if curr + d in nums:
-                curr += d
-                cnts[d] += 1
-                found = True
-                break
-        if not found:
-            break
-    ans1 = cnts[1] * cnts[3]
+    nums = sorted(arr)
+    diffs = [b - a for a, b in zip(nums, nums[1:])]
+    ans1 = diffs.count(1) * diffs.count(3)
+    ctx.submit(1, ans1)
 
-    nums.add(0)
-    nums = list(nums)
-    dp = dict()
+    # Part 2
+    dp = defaultdict(int)
     dp[0] = 1
     for x in nums:
         for d in range(1, 3+1):
             y = x + d
             if y in nums:
-                if y not in dp:
-                    dp[y] = 0
                 dp[y] += dp[x]
-    ans2 = dp[mx]
+    ans2 = dp[max(nums)]
 
-    ctx.submit(1, ans1)
     ctx.submit(2, ans2)
