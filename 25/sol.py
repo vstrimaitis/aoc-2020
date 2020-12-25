5fd62e1e843ccba87a01ee55653d1900aa3f48a3
Vytautas Strimaitis <vstrimaitis@gmail.com>
2020-12-25 07:33:18 +0200

    Improve day 25

--- 25/sol.py
+++ 25/sol.py
@@ -1,14 +1,6 @@
 from puzzle import PuzzleContext
 import math
 
-def pwr(a, e, m):
-    if e == 0:
-        return 1
-    if e % 2 == 1:
-        return pwr(a, e-1, m) * a % m
-    h = pwr(a, e//2, m)
-    return h*h % m
-
 def discrete_log(a, b, p):
     # Finds x such that log_a(x) = b mod p
     n = 1 + int(math.sqrt(p))
@@ -34,10 +26,10 @@ with PuzzleContext(year=2020, day=25) as ctx:
     card_loop = discrete_log(7, card_pub, MOD)
     door_loop = discrete_log(7, door_pub, MOD)
 
-    assert card_pub == pwr(7, card_loop, MOD)
-    assert door_pub == pwr(7, door_loop, MOD)
+    assert card_pub == pow(7, card_loop, MOD)
+    assert door_pub == pow(7, door_loop, MOD)
 
-    encr_key = pwr(card_pub, door_loop, MOD)
-    assert encr_key == pwr(door_pub, card_loop, MOD)
+    encr_key = pow(card_pub, door_loop, MOD)
+    assert encr_key == pow(door_pub, card_loop, MOD)
 
     ctx.submit(1, encr_key)
